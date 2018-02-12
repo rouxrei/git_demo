333f9c5d1f6a663138f89849f37133065fe2bd11
schaeje <jellert.schaepherders@gmail.com>
2018-02-12 22:16:20 +0100

    create branch and push to remote

--- test.py
+++ test.py
@@ -9,3 +9,4 @@ Created on Thu Jan 11 13:51:47 2018
 print("let's try to create a branch called experiment, commit, push and create a pull request")
 print("Jellert test: add something to master")
 print("test in Python io GitGui")
+print("create branch and push to remote")
