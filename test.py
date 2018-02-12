98a53070a4b682d49352b7d2456680d5f6b1e79e
schaeje <jellert.schaepherders@gmail.com>
2018-02-12 22:20:57 +0100

    merge

--- test.py
+++ test.py
@@ -10,4 +10,4 @@ print("let's try to create a branch called experiment, commit, push and create a
 print("Jellert test: add something to master")
 print("test in Python io GitGui")
 print("create branch and push to remote")
-print("now merge then push")
+print("now merge then push sqddfqsdf")
