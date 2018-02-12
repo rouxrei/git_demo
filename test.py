ccd6846002d38ffcb0affe091452b564b9fec03d
Schaepherders Jellert <schaeje@cronos.be>
2018-02-12 21:03:20 +0100

    some more testing

--- test.py
+++ test.py
@@ -3,7 +3,8 @@
 Created on Thu Jan 11 13:51:47 2018
 
 @author: zhangka
+@contributor: schaeje
 """
 
 print("let's try to create a branch called experiment, commit, push and create a pull request")
-print("Jellert test: add something to master branch")
+print("Jellert test: add something to master")
